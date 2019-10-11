7ca6131839c2b000da67bf5595b43467335c24dd
Mondal10 <amitmondal111.am@gmail.com>
2019-10-11 20:48:54 +0530

    Created Navbar component and imported bootstrap, styled-components in packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+import Navbar from './components/layouts/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello Amit Mondal</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import styled from 'styled-components';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav
+          className='navbar navbar-expand-md navbar-dark fixed-top'
+          style={{
+            backgroundColor: '#ef5350',
+            color: '#ffffff',
+          }}
+        >
+          <a className='navbar-brand' href='https://github.com/mondal10'>
+            <img
+              alt='Pokedex logo'
+              src={process.env.PUBLIC_URL + '/assets/logo-48.png'}
+              style={{
+                height: '40px',
+                width: '40px'
+              }}
+            />
+            Pokedex
+          </a>
+        </nav>
+      </div >
+    );
+  }
+}
+
+export default Navbar;
