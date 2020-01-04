cd0d217c3a75746acf96becd55a92afb93fa8aff
Mondal10 <amitmondal111.am@gmail.com>
2020-01-04 21:31:07 +0530

    adding moves list to the move collapsable

--- src/components/pokemon/PokemonMoves.js
+++ src/components/pokemon/PokemonMoves.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 
+import Utility from '../../Utility';
+
 class PokemonMoves extends Component {
 
   render() {
     const { moves } = this.props;
 
-    // moves.forEach(info => {
-    //   console.log(info.move.name, info.move.url);
-    //   console.log(info.move.url.split('/')[6]);
-    // });
+    // # Todo:
+    // Display data on click pop-over
+    // Fetch each moves data(accuracy, pp, power, type) using moves->url
 
     return (
-      <div>
+      <div className="row">
         {
-          moves.map(info => <div key={info.move.url.split('/')[6]}>{info.move.name} | {info.move.url}</div>)
+          moves.map(info => {
+            return (
+              <div className="mr-2 mb-2" key={info.move.url.split('/')[6]}>
+                <button type="button" className="btn btn-outline-dark">
+                  {Utility.toCapitalize(info.move.name)}
+                </button>
+              </div>
+            );
+          })
         }
       </div>
     );
